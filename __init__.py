3ec387296ad8e970119473282b564cb0c4d9f450
Pixylz <Pixylz@Naina-Bhatia.local>
2018-05-08 18:06:01 +0530

    changes for server

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ import requests
 #end of import statements
 
 #google plus login credentials
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('/var/www/restaurants/restaurants/client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Restaurant Project"
 
 
@@ -64,7 +64,7 @@ def fbconnect():
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open('/var/www/restaurants/restaurants/fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
